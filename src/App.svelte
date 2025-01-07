d44c8df8101ff4127b3ba2d0ad4f4b322e08ad9b
Fatih Emre Aksoy <116756659+enigma0x1@users.noreply.github.com>
2025-01-07 03:40:47 +0300

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,36 @@
+// src/App.svelte
 <script>
     import { onMount } from 'svelte';
     import maplibregl from 'maplibre-gl';
     import 'maplibre-gl/dist/maplibre-gl.css';
 
     onMount(() => {
-        new maplibregl.Map({
+        const map = new maplibregl.Map({
             container: 'map',
-            style: 'https://demotiles.maplibre.org/style.json',
+            style: {
+                version: 8,
+                sources: {
+                    'simple-tiles': {
+                        type: 'raster',
+                        tiles: ['https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png'],
+                        tileSize: 256
+                    }
+                },
+                layers: [{
+                    id: 'simple-tiles',
+                    type: 'raster',
+                    source: 'simple-tiles',
+                    minzoom: 0,
+                    maxzoom: 22
+                }]
+            },
             center: [0, 0],
             zoom: 2
+        });
+
+        // Harita yüklendiğinde sepia filtresi ekle
+        map.on('load', () => {
+            map.getCanvas().style.filter = 'sepia(20%) brightness(105%)';
         });
     });
 </script>
@@ -16,8 +38,17 @@
 <div id="map"></div>
 
 <style>
-    #map { 
-        width: 100%; 
-        height: 100vh; 
+    :global(body) {
+        margin: 0;
+        padding: 0;
+        background-color: #f5f5f5;
+    }
+
+    #map {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        width: 100%;
+        height: 100%;
     }
 </style>
