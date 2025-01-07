6388e7545aad1473a59fe5ee014651d847658782
Fatih Emre Aksoy <116756659+enigma0x1@users.noreply.github.com>
2025-01-07 03:45:51 +0300

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -9,27 +9,23 @@
             style: {
                 version: 8,
                 sources: {
-                    'simple-tiles': {
+                    osm: {
                         type: 'raster',
-                        tiles: ['https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png'],
+                        tiles: ['https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'],
                         tileSize: 256,
                         attribution: '&copy; OpenStreetMap Contributors'
                     }
                 },
                 layers: [{
-                    id: 'simple-tiles',
+                    id: 'osm',
                     type: 'raster',
-                    source: 'simple-tiles',
+                    source: 'osm',
                     minzoom: 0,
-                    maxzoom: 22
+                    maxzoom: 19
                 }]
             },
             center: [0, 0],
             zoom: 2
-        });
-
-        map.on('load', () => {
-            map.getCanvas().style.filter = 'sepia(20%) brightness(105%)';
         });
     });
 </script>
@@ -40,14 +36,10 @@
     :global(body) {
         margin: 0;
         padding: 0;
-        background-color: #f5f5f5;
     }
 
     #map {
-        position: absolute;
-        top: 0;
-        bottom: 0;
         width: 100%;
-        height: 100%;
+        height: 100vh;
     }
 </style>
